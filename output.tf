5511230b581495e0631974e457a7959d133623d4
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-09-04 17:49:48 -0700

    init

--- output.tf
+++ output.tf
@@ -3,5 +3,6 @@ output "nsg_name" {
                             azurerm_network_security_group.master.*.name,
                             azurerm_network_security_group.agent.*.name,
                             azurerm_network_security_group.public_agent.*.name,
+                            azurerm_network_security_group.vnet_public_subnet.*.name,
                             list("")), 0)}"
 }
