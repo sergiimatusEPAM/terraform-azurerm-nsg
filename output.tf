6055385718d18030b9844717879df555fc89711a
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-09-06 10:11:40 -0700

    init

--- output.tf
+++ output.tf
@@ -1,8 +1,19 @@
+# Network Security Group Name
 output "nsg_name" {
   value = "${element(concat(azurerm_network_security_group.bootstrap.*.name,
                             azurerm_network_security_group.master.*.name,
                             azurerm_network_security_group.agent.*.name,
                             azurerm_network_security_group.public_agent.*.name,
                             azurerm_network_security_group.vnet_public_subnet.*.name,
+                            list("")), 0)}"
+}
+
+# Network Security Group ID
+output "nsg_id" {
+  value = "${element(concat(azurerm_network_security_group.bootstrap.*.id,
+                            azurerm_network_security_group.master.*.id,
+                            azurerm_network_security_group.agent.*.id,
+                            azurerm_network_security_group.public_agent.*.id,
+                            azurerm_network_security_group.vnet_public_subnet.*.id,
                             list("")), 0)}"
 }
