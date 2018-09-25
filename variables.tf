db0d9c5d5925d434344673095b246c8c31351a23
Lukas Loesche <lukas@mesosphere.io>
2018-09-25 14:03:39 +0200

    Automatic update of variable descriptions

--- variables.tf
+++ variables.tf
@@ -1,22 +1,32 @@
 # Cluster Name
-variable "name_prefix" {}
+variable "name_prefix" {
+  description = "Cluster Name"
+}
 
 # Format the hostname inputs are index+1, region, name_prefix
 variable "hostname_format" {
+  description = "Format the hostname inputs are index+1, region, cluster_name"
   default = "nsg-%[1]d-%[2]s"
 }
 
 # Name of the azure resource group
-variable "resource_group_name" {}
+variable "resource_group_name" {
+  description = "resource group name"
+}
 
 # Specify dcos role for nsg configuration
-variable "dcos_role" {}
+variable "dcos_role" {
+  description = "dcos role"
+}
 
 # Location (region)
-variable "location" {}
+variable "location" {
+  description = "location"
+}
 
 # Add special tags to the resources created by this module
 variable "tags" {
+  description = "Add custom tags to all resources"
   type    = "map"
   default = {}
 }
