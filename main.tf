d3ffddf66b590fd0b8d9f207ad8c688426ac0dcd
Lukas Loesche <lukas@mesosphere.io>
2018-09-27 16:21:12 +0200

    Add documentation header to main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,7 @@
+/**
+ * [![Build Status](https://jenkins-terraform.mesosphere.com/service/dcos-terraform-jenkins/job/dcos-terraform/job/terraform-azurerm-nsg/job/master/badge/icon)](https://jenkins-terraform.mesosphere.com/service/dcos-terraform-jenkins/job/dcos-terraform/job/terraform-azurerm-nsg/job/master/)
+ * # azurerm nsg
+ * The module creates DC/OS Network Security Groups per DC/OS role on AzureRM.
+ */
+
 provider "azurerm" {}
