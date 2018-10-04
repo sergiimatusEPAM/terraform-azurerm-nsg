a73c10c7016ed22a086445da6ec2f92bd8e8ae6c
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2018-10-04 15:37:25 -0700

    updating the main.tf with the proper readme

--- main.tf
+++ main.tf
@@ -2,6 +2,19 @@
  * [![Build Status](https://jenkins-terraform.mesosphere.com/service/dcos-terraform-jenkins/job/dcos-terraform/job/terraform-azurerm-nsg/job/master/badge/icon)](https://jenkins-terraform.mesosphere.com/service/dcos-terraform-jenkins/job/dcos-terraform/job/terraform-azurerm-nsg/job/master/)
  * # azurerm nsg
  * The module creates DC/OS Network Security Groups per DC/OS role on AzureRM.
+ *
+ * ## EXAMPLE
+ *
+ * ```hcl
+ * module "dcos-security-groups" {
+ *   source  = "dcos-terraform/nsg/azurerm"
+ *   version = "~> 0.1"
+ *
+ *   dcos_role = "master"
+ *   resource_group_name = "test"
+ *   location = "West US"
+ * }
+ * ```
  */
 
 provider "azurerm" {}
