2f1c93aee9b160801f17833e33b899b0ad012bea
Weston Bassler <wbassler@mesosphere.com>
2018-10-23 16:36:11 -0400

    update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,47 +1,39 @@
 output "bootstrap.nsg_name" {
   description = "nsg name"
-
-  value = "${azurerm_network_security_group.bootstrap.name}"
+  value       = "${azurerm_network_security_group.bootstrap.name}"
 }
 
 output "bootstrap.nsg_id" {
   description = "nsg id"
-
-  value = "${azurerm_network_security_group.bootstrap.id}"
+  value       = "${azurerm_network_security_group.bootstrap.id}"
 }
 
 output "masters.nsg_name" {
   description = "nsg name"
-
-  value = "${azurerm_network_security_group.masters.name}"
+  value       = "${azurerm_network_security_group.masters.name}"
 }
 
 output "masters.nsg_id" {
   description = "nsg id"
-
-  value = "${azurerm_network_security_group.masters.id}"
+  value       = "${azurerm_network_security_group.masters.id}"
 }
 
 output "private_agents.nsg_name" {
   description = "nsg name"
-
-  value = "${azurerm_network_security_group.private_agents.name}"
+  value       = "${azurerm_network_security_group.private_agents.name}"
 }
 
 output "private_agents.nsg_id" {
   description = "nsg id"
-
-  value = "${azurerm_network_security_group.private_agents.id}"
+  value       = "${azurerm_network_security_group.private_agents.id}"
 }
 
 output "public_agents.nsg_name" {
   description = "nsg name"
-
-  value = "${azurerm_network_security_group.public_agents.name}"
+  value       = "${azurerm_network_security_group.public_agents.name}"
 }
 
 output "public_agents.nsg_id" {
   description = "nsg id"
-
-  value = "${azurerm_network_security_group.public_agents.id}"
+  value       = "${azurerm_network_security_group.public_agents.id}"
 }
