3f2b65cb82c6d0858409da00f009eff4cdd0760a
Sebastian Brandt <793580+sebbrandt87@users.noreply.github.com>
2019-06-27 22:37:16 +0200

    chore: updated variable descriptions with tfdescsan

--- outputs.tf
+++ outputs.tf
@@ -1,39 +1,39 @@
 output "bootstrap.nsg_name" {
-  description = "nsg name"
+  description = "Network security group name of the bootstrap"
   value       = "${azurerm_network_security_group.bootstrap.name}"
 }
 
 output "bootstrap.nsg_id" {
-  description = "nsg id"
+  description = "Network security group id of the bootstrap"
   value       = "${azurerm_network_security_group.bootstrap.id}"
 }
 
 output "masters.nsg_name" {
-  description = "nsg name"
+  description = "Network security group name of the masters"
   value       = "${azurerm_network_security_group.masters.name}"
 }
 
 output "masters.nsg_id" {
-  description = "nsg id"
+  description = "Network security group id of the masters"
   value       = "${azurerm_network_security_group.masters.id}"
 }
 
 output "private_agents.nsg_name" {
-  description = "nsg name"
+  description = "Network security group name of the private agents"
   value       = "${azurerm_network_security_group.private_agents.name}"
 }
 
 output "private_agents.nsg_id" {
-  description = "nsg id"
+  description = "Network security group id of the private agents"
   value       = "${azurerm_network_security_group.private_agents.id}"
 }
 
 output "public_agents.nsg_name" {
-  description = "nsg name"
+  description = "Network security group name of the public agents"
   value       = "${azurerm_network_security_group.public_agents.name}"
 }
 
 output "public_agents.nsg_id" {
-  description = "nsg id"
+  description = "Network security group id of the public agents"
   value       = "${azurerm_network_security_group.public_agents.id}"
 }
